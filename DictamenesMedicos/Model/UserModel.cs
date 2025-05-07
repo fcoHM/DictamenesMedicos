4463cece5fb29889ff3f12f6f451554ab5e05e4b
Fernando Jesus Nunez Valdez <fer.bsof@outlook.com>
2025-05-07 11:41:14 -0600

    Archivo Base de la Base de datos

--- DictamenesMedicos/Model/UserModel.cs
+++ DictamenesMedicos/Model/UserModel.cs
@@ -8,6 +8,29 @@ namespace DictamenesMedicos.Model
 {
     public class UserModel
     {
-        public int NSS { get; set; }
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string NSS { get; set; }
+        public string Password { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public int Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string TelefonoFijo { get; set; }
+        public string TelefonoMovil {  get; set; }
+        public string CorreoElectronico { get; set; }
+        public string CodigoPostal {  get; set; }
+        public string Estado {  get; set; }
+        public string Municipio { get; set; }
+        public string Localidad { get; set; }
+        public string Calle {  get; set; }
+        public string NumeroExterior {  get; set; }
+        public string NumeroInterior {  get; set; }
+        public string Descripcion {  get; set; }
+        public int TipoSangre {  get; set; }
+        public string EnfermedadesCronicas {  get; set; }
+        public string Alergias {  get; set; }
+
+
     }
 }
--- DictamenesMedicos/Repositories/RepositoryBase.cs
+++ DictamenesMedicos/Repositories/RepositoryBase.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DictamenesMedicos.Repositories
 {
-    internal class RepositoryBase
+    public abstract class RepositoryBase
     {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            // Connection. Solo cambiar el nombre de Server
+            _connectionString =
+            "Server = LAPTOP-8DIT8R4T\\VSGESTION; " +
+            "Database=DictamenesMedicos; " +
+            "Integrated Security = true";
+
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
