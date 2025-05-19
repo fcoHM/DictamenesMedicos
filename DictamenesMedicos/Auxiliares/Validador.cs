12cf70d91a162497103c4d7280c7821099fd64b4
Fernando Jesus Nunez Valdez <fer.bsof@outlook.com>
2025-05-18 18:29:03 -0600

    Sign Up con validaciones terminado

---
+++ DictamenesMedicos/Auxiliares/Validador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DictamenesMedicos.Auxiliares
+{
+    public class Validador
+    {
+        static public bool EsNombreValido(string nombre)
+        {
+            string patron = @"^([A-Za-zÁÉÍÓÚÑáéíóúñ]{3,})(\s[A-Za-zÁÉÍÓÚÑáéíóúñ]{3,})*$";
+            return Regex.IsMatch(nombre, patron);
+        }
+        static public bool EsApellidoValido(string apellido)
+        {
+            string patron = @"^([A-Za-zÁÉÍÓÚÑáéíóúñ]{3,})(\s[A-Za-zÁÉÍÓÚÑáéíóúñ]{2,})*$";
+            return Regex.IsMatch(apellido, patron);
+        }
+
+        static public bool EsNumeroTelefonoValido(string numero)
+        {
+            string patron = @"^\d{10}$";
+            return Regex.IsMatch(numero, patron);
+        }
+        static public bool FechaNacimientoValida(DateTime fechaNac) // Si es mayor a 15 años fechaNac
+        {
+            return (new DateTime(2009, 12, 31)) > fechaNac;
+        }
+
+        static public bool EsCorreoValido(string correo)
+        {
+            string patron = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(correo, patron);
+        }
+
+        static public bool EsNSSValido(string nss)
+        {
+            string patron = @"^\d{11}$";
+            return Regex.IsMatch(nss, patron);
+        }
+
+        static public bool EsPasswordValida(SecureString strPass)
+        {
+            string texto = SecureStringHasher.SecureStringToString(strPass);
+            return string.IsNullOrWhiteSpace(texto) == false && texto.Length >= 8;
+        }
+
+        public static bool EsCodigoPostalValido(string codigoPostal)
+        {
+            return Regex.IsMatch(codigoPostal, @"^\d{5}$");
+        }
+
+        // Texto no vacio
+        public static bool EstaVacia(string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto);
+        }
+    }
+}
---
+++ DictamenesMedicos/Auxiliares/VentanasError.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace DictamenesMedicos.Auxiliares
+{
+    public class VentanasError
+    {
+        static public void ShowErrorVentana(string errorMessage)
+        {
+            // Mensaje
+            var messageText = new TextBlock
+            {
+                Text = errorMessage,
+                Margin = new Thickness(20),
+                TextWrapping = TextWrapping.Wrap,
+                Foreground = Brushes.DarkRed,
+                FontSize = 14,
+                FontWeight = FontWeights.SemiBold
+            };
+
+            // Boton cerrar
+            var closeButton = new Button
+            {
+                Content = "Cerrar",
+                Width = 80,
+                Height = 30,
+                Margin = new Thickness(0, 10, 0, 0),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Background = Brushes.IndianRed,
+                Foreground = Brushes.White,
+                FontWeight = FontWeights.Bold
+            };
+
+            // Contenedor
+            var stackPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Margin = new Thickness(20)
+            };
+
+            stackPanel.Children.Add(messageText);
+            stackPanel.Children.Add(closeButton);
+
+            // Crear la ventana de error
+            var errorWindow = new Window
+            {
+                Title = "❌ Error",
+                Content = stackPanel,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ResizeMode = ResizeMode.NoResize,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                WindowStyle = WindowStyle.ToolWindow,
+                Background = Brushes.White,
+                Topmost = true
+            };
+
+            // Funcionalidad Cerrar
+            closeButton.Click += (s, e) => errorWindow.Close();
+
+            errorWindow.ShowDialog();
+        }
+    }
+}
